7e0031d6b205ca372d83e98e6716aceb36472ad0
Sagar Patel <17.sagar.patel@gmail.com>
2012-09-29 02:12:42 -0400

    Grid aligned, need to get touch input next.

--- Q_Grid_UnityFolder/Assets/Scripts/CollumnsGenerator.cs
+++ Q_Grid_UnityFolder/Assets/Scripts/CollumnsGenerator.cs
@@ -1,15 +1,45 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class CollumnsGenerator : MonoBehaviour {
+
+public class CollumnsGenerator : MonoBehaviour 
+{
+	public GameObject collumnmObject;
+	public List<GameObject> collumnsList;
+	public int collumnsCount = 10;
+	public int distanceBetweenRows = 10;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
+
+		collumnsList = new List<GameObject>();
+		for(int i = 0; i < collumnsCount; i++)
+		{
+			
+			collumnsList.Add((GameObject)Instantiate(collumnmObject, transform.localPosition, transform.localRotation));
+			
+			// set as child of generator
+			collumnsList[i].transform.parent = transform;
+
+			// set distance between, taking into account scale
+			float tempSize = collumnsList[i].transform.localScale.x;
+			Vector3 tempPosition = collumnsList[i].transform.localPosition;
+			float newX = i * tempSize + i*distanceBetweenRows;
+			tempPosition.x = newX;
+			collumnsList[i].transform.localPosition = tempPosition;
+
+		}
+	
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 	
 	}
+
+
 }
